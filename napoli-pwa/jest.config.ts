ef1ba0e9e7bd0d1f50183e83695b5b03718d21e3
Raz <razpodesta@gmail.com>
2025-10-29 16:49:46 -0300

    first commit

---
+++ napoli-pwa/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: 'napoli-pwa',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/napoli-pwa',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
